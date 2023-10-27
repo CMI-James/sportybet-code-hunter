33ffe79edc5ac6e6276a88c970770c432e96ad9a
AntiVlad <vladnyash@gmail.com>
2023-10-27 23:40:31 +0100

    Update

--- main.js
+++ main.js
@@ -1,106 +1,103 @@
 const puppeteer = require('puppeteer');
 
+async function extractTextFromElement(element) {
+  const text = await element.evaluate((el) => el.textContent);
+  return text.trim(); // Trim any leading/trailing whitespace
+}
+
+async function extractWordsFromText(text, regexPattern) {
+  const matches = text.match(regexPattern);
+  if (matches) {
+    return matches.map((match) => match.trim());
+  }
+  return [];
+}
+
+async function extractAllWordsFromDivElements(page, selector, regexPattern) {
+  const divElements = await page.$$(selector);
+  const extractedWords = [];
+
+  for (const divElement of divElements) {
+    const textContent = await extractTextFromElement(divElement);
+    const words = await extractWordsFromText(textContent, regexPattern);
+    extractedWords.push(...words);
+  }
+
+  return extractedWords;
+}
+
+async function extractAllTextFromDivElements(page, selector) {
+  const divElements = await page.$$(selector);
+  const extractedText = [];
+
+  for (const divElement of divElements) {
+    const allText = await divElement.$$eval('*', (elements) => {
+      return elements.map((el) => el.textContent);
+    });
+
+    extractedText.push(allText.join(' '));
+  }
+
+  return extractedText;
+}
+async function scrollDownForDuration(page, duration) {
+  const start = Date.now();
+  while (Date.now() - start < duration) {
+    await page.evaluate(() => {
+      window.scrollBy(0, window.innerHeight);
+    });
+    await page.waitForTimeout(1000); // Adjust the delay to control scrolling speed
+  }
+}
 (async () => {
   const browser = await puppeteer.launch({ headless: false, executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" });
   const page = await browser.newPage();
 
   await page.setDefaultNavigationTimeout(0);
-
-  await page.goto('https://twitter.com/search?q=sportybet&src=typed_query&f=live');
+  const cookies = require('./auth.json');
+  await page.setCookie(...cookies);
+  await page.goto('https://twitter.com/AcedTips');
 
   
-  {
-    const targetPage = page;
-    const element = await waitForSelectors([["aria/Login"],["#j_page_header > div.m-top-wrapper > div > div.m-logo-bar > div.m-login-bar > div.m-opt > div.m-psd-wrapper > div.m-psd > button"]], targetPage, { timeout, visible: true });
-    await scrollIntoViewIfNeeded(element, timeout);
-    await element.click({
-      offset: {
-        x: 33.5,
-        y: 13,
-      },
-    });
-  }
-  console.log("Logged in");
+  const selector = 'div[class="css-1dbjc4n r-1iusvr4 r-16y2uox r-1777fci r-kzbkwu"]'; // Replace with your desired CSS selector
+  await page.waitForSelector(selector)
+  const allTextFromDivElements = await extractAllTextFromDivElements(page, selector);
+  await scrollDownForDuration(page, 30000); // 30 seconds
+  console.log(allTextFromDivElements)
 
-  for (let i = 0; i < values.length; i++) {
-    const betCode = values[i];
+  console.log(typeof(allTextFromDivElements))
+  const regex = /\b(?=[A-Za-z]*\d)(?=\d*[A-Za-z])[\w\d]{5,9}\b/g;
 
-    await page.waitForSelector('#j_betslip > div.m-betslips > div.m-betslip-search > div.m-opt-wrapper > div.m-input-wrapper > span > input');
+  const text = allTextFromDivElements[0];
 
-    await page.type('#j_betslip > div.m-betslips > div.m-betslip-search > div.m-opt-wrapper > div.m-input-wrapper > span > input', betCode);
-    // console.log("Typed code");
+ const extractedData = text.match(regex);
+ if (extractedData) {
+  console.log("Extracted Data:", extractedData);
+} else {
+  console.log("No matches found.");
+}
 
-    await page.click('#j_betslip > div.m-betslips > div.m-betslip-search > div.m-opt-wrapper > button');
-    // console.log("Loaded code");
+  // Extract and append new text content
+  // const newContent = await extractAllWordsFromDivElements(page, selector, regex);
+  // console.log(newContent);
 
-    
-      await page.keyboard.down('Control');
-      await page.keyboard.press('A');
-      await page.keyboard.up('Control');
-      await page.keyboard.press('Backspace');
-      await page.type('#j_stake_0 > span > input', stake);
-      // console.log("Input stake");
+  // // Scroll down for 30 seconds
+  // await scrollDownForDuration(page,5000 ); // 30 seconds
 
-      await page.waitForTimeout(3000);
-    {
-        const targetPage = page;
-        const element = await waitForSelectors([["#j_betslip > div.m-betslips > div:nth-child(2) > div > div.m-stake > div > div.m-btn-wrapper > button"]], targetPage, { timeout, visible: true });
-        await scrollIntoViewIfNeeded(element, timeout);
-        await element.click({
-          offset: {
-            x: 32,
-            y: 5,
-          },
-        });
-    }
-    // console.log("Accept/Place Bet 1")
-    await page.waitForTimeout(2000);
-    {
-        const targetPage = page;
-        const element = await waitForSelectors([["#j_betslip > div.m-betslips > div:nth-child(2) > div > div.m-stake > div > div.m-btn-wrapper > button"]], targetPage, { timeout2, visible: true });
-        await scrollIntoViewIfNeeded(element, 2000);
-        await element.click({
-          offset: {
-            x: 32,
-            y: 5,
-          },
-        });
-    }
-    // console.log("Accept/Place Bet 2")
+  // // Extract and append new words
+  // const newWords = await extractAllWordsFromDivElements(page, selector, regex);
+  // console.log(newWords);
+  
+  // for (const str of allTextFromDivElements) {
+  //   const matches = str.match(regex); // Use match to find all matches in the string
+  //   if (matches) {
+  //     console.log("Matches in string:", str);
+  //     console.log(matches);
+  //   }
+  // }
+  
 
-    {
-        const targetPage = page;
-        const element = await waitForSelectors([["#j_betslip > div.m-betslips > div:nth-child(2) > div > div.m-stake > div > div.m-comfirm-wrapper > div > div.m-btn-wrapper > button.af-button.af-button--primary"]], targetPage, { timeout2, visible: true });
-        await scrollIntoViewIfNeeded(element, 2000);
-        await element.click({
-          offset: {
-            x: 32,
-            y: 5,
-          },
-        });
-    }
-    // console.log("Confirm")
 
-    await page.waitForTimeout(3000);
-
-    {
-        const targetPage = page;
-        const element = await waitForSelectors([["aria/OK"],["#esDialog0 > div.es-dialog.m-dialog > div > div > div > div.m-pop-main > div.m-btn-wrapper.m-ok-wrap > button"]], targetPage, { timeout2, visible: true });
-        await scrollIntoViewIfNeeded(element, 4000);
-        await element.click({
-          offset: {
-            x: 141.5,
-            y: 21,
-          },
-        });
-    }
-    // console.log("Okay Dialog")
-
-    console.log(`Staked ${betCode}`);
-
-  }
-
-  await browser.close();
 })();
 
 async function waitForSelectors(selectors, frame, options) {
